ebf9584ebc92b584a4e65e8b2320df2ddfcef6a8
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-08-03 19:38:03 +0300

    added animation during swipping my app

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,32 @@
 import { Header } from 'components/Header/Header';
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 function SharedLayout() {
+  const location = useLocation();
+
+  const transitions = useTransition(location.pathname, {
+    from: {
+      transform: 'translateX(100%)',
+    },
+    enter: {
+      transform: 'translateX(0%)',
+    },
+    leave: {
+      transform: 'translateX(-100%)',
+    },
+  });
+
   return (
     <>
       <Header />
       <Suspense fallback={<div>Loading ...</div>}>
-        <Outlet />
+        {transitions((props, item, key) => (
+          <animated.div key={key} style={props}>
+            <Outlet />
+          </animated.div>
+        ))}
       </Suspense>
     </>
   );
