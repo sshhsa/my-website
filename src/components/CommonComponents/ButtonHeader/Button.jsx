44e26351445aa15f6c3fe3b24673a28ac62a5b2a
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-08-04 16:56:32 +0300

    added active class for header-buttons

--- src/components/CommonComponents/ButtonHeader/Button.jsx
+++ src/components/CommonComponents/ButtonHeader/Button.jsx
@@ -2,9 +2,19 @@ import PropTypes from 'prop-types';
 import scss from './Button.module.scss';
 import { NavLink } from 'react-router-dom';
 
-export const ButtonHeader = ({ buttonValue, link }) => {
+export const ButtonHeader = ({ buttonValue, link, active, onClick }) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <NavLink to={link} className={scss.buttonHeader}>
+    <NavLink
+      to={link}
+      className={`${scss.buttonHeader} ${active ? scss.active : ''}`}
+      onClick={handleClick}
+    >
       {buttonValue}
     </NavLink>
   );
@@ -13,4 +23,6 @@ export const ButtonHeader = ({ buttonValue, link }) => {
 ButtonHeader.propTypes = {
   link: PropTypes.string,
   buttonValue: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,58 @@
+import React, { useState } from 'react';
 import { Container } from 'components/CommonComponents/Container/Container';
 import { ButtonHeader } from 'components/CommonComponents/ButtonHeader/Button';
 import scss from './Header.module.scss';
 
 export const Header = () => {
+  const [activeButton, setActiveButton] = useState('Home');
+
+  const handleNavLinkClick = buttonValue => {
+    setActiveButton(buttonValue);
+  };
+
   return (
     <header className={scss.sectionCeil}>
       <Container>
         <ul className={scss.headerList}>
           <li className={scss.headerItem}>
-            <ButtonHeader buttonValue="Home" link="/" />
+            <ButtonHeader
+              buttonValue="Home"
+              link="/"
+              active={activeButton === 'Home'}
+              onClick={() => handleNavLinkClick('Home')}
+            />
           </li>
           <li className={scss.headerItem}>
-            <ButtonHeader buttonValue="About me" link="/about" />
+            <ButtonHeader
+              buttonValue="About me"
+              link="/about"
+              active={activeButton === 'About'}
+              onClick={() => handleNavLinkClick('About')}
+            />
           </li>
           <li className={scss.headerItem}>
-            <ButtonHeader buttonValue="Skills" link="/skills" />
+            <ButtonHeader
+              buttonValue="Skills"
+              link="/skills"
+              active={activeButton === 'Skills'}
+              onClick={() => handleNavLinkClick('Skills')}
+            />
           </li>
           <li className={scss.headerItem}>
-            <ButtonHeader buttonValue="Portfolio" link="/portfolio" />
+            <ButtonHeader
+              buttonValue="Portfolio"
+              link="/portfolio"
+              active={activeButton === 'Portfolio'}
+              onClick={() => handleNavLinkClick('Portfolio')}
+            />
           </li>
           <li className={scss.headerItem}>
-            <ButtonHeader buttonValue="Contact Me" link="/contact" />
+            <ButtonHeader
+              buttonValue="Contact Me"
+              link="/contact"
+              active={activeButton === 'Contact'}
+              onClick={() => handleNavLinkClick('Contact')}
+            />
           </li>
         </ul>
       </Container>
