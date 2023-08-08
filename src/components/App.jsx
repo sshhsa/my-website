f2124950ab0d3b36393d32622deeda90182c80ae
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-08-08 18:02:33 +0300

    refacted conflicts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
-import { Scrollbars } from 'react-custom-scrollbars';
 
 import { Hero } from './Hero/Hero';
 import { Skills } from './Skills/Skills';
@@ -11,19 +10,15 @@ import { ContactMe } from './ContactMe/ContactMe';
 
 export const App = () => {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <Scrollbars style={{ width: '100%', height: '100%' }}>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Hero />} />
-            <Route path="about" element={<AboutMe />} />
-            <Route path="skills" element={<Skills />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="contact" element={<ContactMe />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Scrollbars>
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Hero />} />
+        <Route path="about" element={<AboutMe />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<ContactMe />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
