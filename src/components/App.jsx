ec97f6f851126d64dd621848ac51f6123422bf65
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-08-10 13:26:09 +0300

    installed library react-custom-scrollbars-2 for beauty scrollbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 import Hero from './Hero/Hero';
 import Skills from './Skills/Skills';
@@ -10,15 +11,19 @@ import NotFound from './NotFound/NotFound';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Hero />} />
-        <Route path="about" element={<AboutMe />} />
-        <Route path="skills" element={<Skills />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="contact" element={<ContactMe />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Scrollbars style={{ width: '100%', height: '100%' }}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Hero />} />
+            <Route path="about" element={<AboutMe />} />
+            <Route path="skills" element={<Skills />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="contact" element={<ContactMe />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Scrollbars>
+    </div>
   );
 };
