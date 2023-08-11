3fca9e54acb836efac57ea0e059857176a89bd78
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-08-11 12:40:16 +0300

    added lazy loading for NotFound page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
 import { Scrollbars } from 'react-custom-scrollbars-2';
+import { lazy } from 'react';
 
 import Hero from './Hero/Hero';
 import Skills from './Skills/Skills';
 import AboutMe from './AboutMe/AboutMe';
 import Portfolio from './Portfolio/Portfolio';
 import ContactMe from './ContactMe/ContactMe';
-import NotFound from './NotFound/NotFound';
+// import NotFound from './NotFound/NotFound';
+
+// const Skills = lazy(() => './Skills/Skills');
+// const AboutMe = lazy(() => './AboutMe/AboutMe');
+// const Portfolio = lazy(() => import('./Portfolio/Portfolio'));
+// const ContactMe = lazy(() => import('./ContactMe/ContactMe'));
+// const NotFound = lazy(() => import('./NotFound/NotFound'));
+const NotFound = lazy(() => import('./NotFound/NotFound'));
 
 export const App = () => {
   return (
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,14 @@
 import Container from 'components/CommonComponents/Container/Container';
+import ButtonNavLink from 'components/CommonComponents/ButtonNavLink/ButtonNavLink';
 import scss from './NotFound.module.scss';
 
 const NotFound = () => {
   return (
     <section className={scss.sectionNotFound}>
-      <Container>Not Found Page</Container>
+      <Container>
+        Page is not found
+        <ButtonNavLink text="Back to Home" link="/" />
+      </Container>
     </section>
   );
 };
